03adf4c509e6907c7f303bd1743542c3a1750ae8
Jackie Atwood <takalaj@verizon.net>
2020-04-22 22:56:09 -0400

    Working on MVP, cookies?

--- users/users-router.js
+++ users/users-router.js
@@ -1,13 +1,15 @@
 const router = require("express").Router();
 
 const Users = require("./users-model.js");
+const restricted = require("../auth/requires-auth-middleware.js"); //this is the middleware that restricts access to any of the routes
 
-router.get("/", (req, res) => {
-  Users.find()
+router.get("/", restricted, (req, res) => {
+  Users.findUsers()
     .then(users => {
-      res.json(users);
+      res.status(200).json(users);
     })
-    .catch(err => res.send(err));
+    .catch(err => 
+        res.status(404).json({ errorMessage: "You shant pass", err }));
 });
 
 
